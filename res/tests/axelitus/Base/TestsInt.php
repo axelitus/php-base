a2f76807e620feee052462a7069d5c01ed4a58ab
Axel Pardemann <axelitusdev@gmail.com>
2013-08-07 21:41:25 -0500

    Added the Int primitive along with the concrete class and the tests.

---
+++ res/tests/axelitus/Base/TestsInt.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Part of composer package: axelitus/base
+ *
+ * @package     axelitus\Base
+ * @version     0.1
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @license     MIT License
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/axelitus/base
+ */
+
+namespace axelitus\Base;
+
+class TestsInt extends TestCase
+{
+    public function test_is_int()
+    {
+        $this->assertTrue(Int::is(0), "The value 0 is not recognized as an int.");
+        $this->assertTrue(Int::is(5), "The value 5 is not recognized as an int.");
+        $this->assertTrue(Int::is(-23), "The value -23 is not recognized as an int.");
+        $this->assertTrue(Int::is(0.0), "The value 0.0 is not recognized as an int.");
+        $this->assertTrue(Int::is(7.0), "The value 7.0 is not recognized as an int.");
+        $this->assertTrue(Int::is(-23.0), "The value -23.0 is not recognized as an int.");
+        $this->assertTrue(Int::is("65"), "The value \"65\" is not recognized as an int.");
+        $this->assertFalse(Int::is(5.23), "The value 5.0 is incorrectly recognized as an int.");
+        $this->assertFalse(Int::is(-4.9), "The value -4.9 is incorrectly recognized as an int.");
+        $this->assertFalse(Int::is("string"), "The value \"string\" is incorrectly recognized as an int.");
+        $this->assertFalse(Int::is("2. string"), "The value \"2. string\" is incorrectly recognized as an int.");
+        $this->assertFalse(Int::is("7. string 9"), "The value \"7. string 9\" is incorrectly recognized as an int.");
+        $this->assertFalse(Int::is("string 4"), "The value \"string 4\" is incorrectly recognized as an int.");
+        $this->assertFalse(Int::is([]), "The value [] is incorrectly recognized as an int.");
+    }
+}
