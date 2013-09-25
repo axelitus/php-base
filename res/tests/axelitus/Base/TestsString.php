4e8917e5ee5750e7e4037c78c5894f0385da78e8
Axel Pardemann <axelitusdev@gmail.com>
2013-09-25 17:01:50 -0500

    Added more coverage to the String tests.

--- res/tests/axelitus/Base/TestsString.php
+++ res/tests/axelitus/Base/TestsString.php
@@ -14,7 +14,7 @@ namespace axelitus\Base;
 
 /**
  * Class TestsString
- * 
+ *
  * @package axelitus\Base
  */
 class TestsString extends TestCase
@@ -80,10 +80,49 @@ class TestsString extends TestCase
     }
 
     /**
-     * Tests the String::beginsWith() function.
+     * Tests the String::pos() function.
      * @depends test_isString
      */
-    public function test_begins_with()
+    public function test_pos()
+    {
+        $expected = 13;
+        $output = String::pos("It is known, Khaleesi.", "Khal");
+        $this->assertEquals($expected, $output, "The string \"Khal\" is not found at position 13 in the string \"It is known, Khaleesi.\".");
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::pos(false, 'string');
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::pos('string', false);
+    }
+
+    /**
+     * Tests the String::contains() function.
+     * @depends test_isString
+     * @depends test_pos
+     */
+    public function test_contains()
+    {
+        $this->assertTrue(String::contains("It is known, Khaleesi.", "Khal"), "The string \"It is known, Khaleesi.\" does not contain the string \"Khal\".");
+        $this->assertFalse(String::contains("It is known, Khaleesi.", "khal"), "The string \"It is known, Khaleesi.\" does not contain the string \"khal\".");
+        $this->assertTrue(String::contains("It is known, Khaleesi.", "khal", false), "The string \"It is known, Khaleesi.\" does not contain the string \"khal\" (case-insensitive).");
+        $this->assertFalse(String::contains("It is known, Khaleesi.", "Chal"), "The string \"It is known, Khaleesi.\" does not contain the string \"Chal\".");
+        $this->assertFalse(String::contains("It is known, Khaleesi.", "Chal", false), "The string \"It is known, Khaleesi.\" does not contain the string \"Chal\".");
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::contains(false, 'string');
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::contains('string', false);
+    }
+
+    /**
+     * Tests the String::beginsWith() function.
+     * @depends test_isString
+     * @depends test_length
+     * @depends test_sub
+     */
+    public function test_beginsWith()
     {
         $this->assertTrue(String::beginsWith("Winter is coming...", "Winter"), "The string \"Winter is coming...\" does not begin with the string \"Winter\".");
         $this->assertTrue(String::beginsWith("Winter is coming...", "winter", false), "The string \"Winter is coming...\" does not begin with the string \"winter\" [case insensitive].");
@@ -99,18 +138,271 @@ class TestsString extends TestCase
     /**
      * Tests the String::endsWith() function.
      * @depends test_isString
+     * @depends test_length
+     * @depends test_sub
      */
-    public function test_ends_with()
+    public function test_endsWith()
     {
-        $this->assertTrue(String::endsWith("It is known, Khaleesi.", "Khaleesi."), "The string \"It is known, Khaleesi.\" does not end with the string \"Khaleesi..\".");
+        $this->assertTrue(String::endsWith("It is known, Khaleesi.", "Khaleesi."), "The string \"It is known, Khaleesi.\" does not end with the string \"Khaleesi.\".");
         $this->assertTrue(String::endsWith("It is known, Khaleesi.", "khaleesi.", false), "The string \"It is known, Khaleesi.\" does not end with the string \"khaleesi.\" [case insensitive].");
         $this->assertFalse(String::endsWith("It is known, Khaleesi.", "khaleesi.", true), "The string \"It is known, Khaleesi.\" does not end with the string \"khaleesi.\" [case sensitive].");
-        $this->assertFalse(String::endsWith("It is known, Khaleesi.", "Khaleesi"), "The string \"It is known, Khaleesi.\" does not end with the string \"Khaleesi\"..");
+        $this->assertFalse(String::endsWith("It is known, Khaleesi.", "Khaleesi"), "The string \"It is known, Khaleesi.\" does not end with the string \"Khaleesi\".");
+        $this->assertTrue(String::endsWith("It is known, Khaleesi.", ""), "The string \"It is known, Khaleesi.\" does not end with the string \"\".");
 
         $this->setExpectedException('\InvalidArgumentException');
         String::endsWith(false, 'string');
 
         $this->setExpectedException('\InvalidArgumentException');
         String::endsWith('string', false);
+    }
+
+    /**
+     * Tests the String::replace() function.
+     * @depends test_isString
+     */
+    public function test_replace()
+    {
+        $expected = "Summer is coming...";
+        $output = String::replace("Winter is coming...", "Winter", "Summer");
+        $this->assertEquals($expected,$output);
+
+        $expected = "Winter is coming...";
+        $output = String::replace("Winter is coming...", "Autumn", "Summer");
+        $this->assertEquals($expected,$output);
+    }
+
+    /**
+     * Tests the String::lower() function.
+     * @depends test_isString
+     */
+    public function test_lower()
+    {
+        $expected = "winter is coming...";
+        $output = String::lower("Winter Is COMING...");
+        $this->assertEquals($expected, $output);
+    }
+
+    /**
+     * Tests the String::upper() function.
+     * @depends test_isString
+     */
+    public function test_upper()
+    {
+        $expected = "WINTER IS COMING...";
+        $output = String::upper("Winter Is COMING...");
+        $this->assertEquals($expected, $output);
+    }
+
+    /**
+     * Tests the String::lcfirst() function.
+     * @depends test_isString
+     */
+    public function test_lcfirst()
+    {
+        $expected = "winter Is COMING...";
+        $output = String::lcfirst("Winter Is COMING...");
+        $this->assertEquals($expected, $output);
+
+        $expected = "wINTER IS COMING...";
+        $output = String::lcfirst("WINTER IS COMING...");
+        $this->assertEquals($expected, $output);
+    }
+
+    /**
+     * Tests the String::ucfirst() function.
+     * @depends test_isString
+     */
+    public function test_ucfirst()
+    {
+        $expected = "Winter Is COMING...";
+        $output = String::ucfirst("winter Is COMING...");
+        $this->assertEquals($expected, $output);
+
+        $expected = "Winter is coming...";
+        $output = String::ucfirst("winter is coming...");
+        $this->assertEquals($expected, $output);
+    }
+
+    /**
+     * Tests the String::ucwords() function.
+     * @depends test_isString
+     */
+    public function test_ucwords()
+    {
+        $expected = "Winter Is Coming...";
+        $output = String::ucwords("winter is coming...");
+        $this->assertEquals($expected, $output);
+
+        $expected = "Winter Is Coming...";
+        $output = String::ucwords("WINTER IS COMING...");
+        $this->assertEquals($expected, $output);
+    }
+
+    /**
+     * Tests the String::isOneOf() function.
+     * @depends test_isString
+     */
+    public function test_isOneOf()
+    {
+        $values = array('apple', 'banana', 'grapes', 'oranges');
+        $this->assertTrue(String::isOneOf('apple', $values));
+        $this->assertFalse(String::isOneOf('Apple', $values));
+        $this->assertTrue(String::isOneOf('Apple', $values, false));
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::isOneOf(false, $values);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::isOneOf('string', array('value', false));
+    }
+
+    /**
+     * Tests the String::studly() function.
+     * @depends test_isString
+     */
+    public function test_studly()
+    {
+        $expected = "winter_is_coming";
+        $output = String::studly("winter_is_coming", array());
+        $this->assertEquals($expected, $output);
+
+        $expected = "WinterIsComing";
+        $output = String::studly("winter_is_coming");
+        $this->assertEquals($expected, $output);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::studly(false);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::studly('separated_string', array(false));
+    }
+
+    /**
+     * Tests the String::camel() function.
+     * @depends test_isString
+     * @depends test_studly
+     */
+    public function test_camel()
+    {
+        $expected = "winter_is_coming";
+        $output = String::camel("winter_is_coming", array());
+        $this->assertEquals($expected, $output);
+
+        $expected = "winterIsComing";
+        $output = String::camel("winter_is_coming");
+        $this->assertEquals($expected, $output);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::camel(false);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::camel('separated_string', array(false));
+    }
+
+    /**
+     * Tests the String::separated() function.
+     * @depends test_isString
+     * @depends test_isOneOf
+     * @depends test_lcfirst
+     * @depends test_ucfirst
+     * @depends test_lower
+     */
+    public function test_separated()
+    {
+        $expected = "winter_Is_Coming";
+        $output = String::separated("winterIsComing");
+        $this->assertEquals($expected, $output);
+
+        $expected = "Winter-Is-Coming";
+        $output = String::separated("WinterIsComing", '-');
+        $this->assertEquals($expected, $output);
+
+        $expected = "winter-is-coming";
+        $output = String::separated("WinterIsComing", '-', 'lower');
+        $this->assertEquals($expected, $output);
+
+        $expected = "WINTER-IS-COMING";
+        $output = String::separated("WinterIsComing", '-', 'upper');
+        $this->assertEquals($expected, $output);
+
+        $expected = "winter-Is-Coming";
+        $output = String::separated("WinterIsComing", '-', 'lcfirst');
+        $this->assertEquals($expected, $output);
+
+        $expected = "Winter-Is-Coming";
+        $output = String::separated("winterIsComing", '-', 'ucfirst');
+        $this->assertEquals($expected, $output);
+
+        $expected = "Winter-is-coming";
+        $output = String::separated("winterIsComing", '-', 'ucwords');
+        $this->assertEquals($expected, $output);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::separated(false);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::separated('camelString', false);
+    }
+
+    /**
+     * Tests the String::truncate() function.
+     * @depends test_isString
+     */
+    public function test_truncate()
+    {
+        $expected = "Winter is ...";
+        $output = String::truncate("Winter is coming!", 10);
+        $this->assertEquals($expected, $output);
+
+        $expected = "Winter is ";
+        $output = String::truncate("Winter is coming!", 10, '');
+        $this->assertEquals($expected, $output);
+
+        $expected = "<span>Winter is ...</span>";
+        $output = String::truncate("<span>Winter is coming!</span>",10, '...', true);
+        $this->assertEquals($expected, $output);
+    }
+
+    /**
+     * Tests the String::nsprintf() function.
+     * @depends test_isString
+     */
+    public function test_nsprintf()
+    {
+        $expected = "This is the name that was replaced: Jon Snow.";
+        $output = String::nsprintf('This is the name that was replaced: %name$s.', array('name' => 'Jon Snow'));
+        $this->assertEquals($expected, $output);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::nsprintf(false);
+    }
+
+    /**
+     * Tests the String::random() function.
+     * @depends test_isString
+     */
+    public function test_random()
+    {
+        $expected = 10;
+        $output = strlen(String::random($expected));
+        $this->assertEquals($expected, $output);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::random(false);
+
+        $this->setExpectedException('\InvalidArgumentException');
+        String::random(10, false);
+    }
+
+    /**
+     * Tests the String::trandom() function.
+     * @depends test_isString
+     * @depends test_random
+     */
+    public function test_trandom()
+    {
+        $expected = 16;
+        $output = strlen(String::trandom());
+        $this->assertEquals($expected, $output);
     }
 }
