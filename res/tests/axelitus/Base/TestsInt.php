047acffb9640dbf4548016c385ad06364e3caeae
Axel Pardemann <axelitusdev@gmail.com>
2013-11-20 13:45:41 -0600

    Added inRange, inside and between funcions to the Int class along with the tests.

--- res/tests/axelitus/Base/TestsInt.php
+++ res/tests/axelitus/Base/TestsInt.php
@@ -70,4 +70,108 @@ class TestsInt extends TestCase
         $this->setExpectedException('\InvalidArgumentException');
         Int::random(6, false);
     }
+
+    /**
+     * Tests Int::inRange()
+     */
+    public function test_inRange()
+    {
+        $value = 3;
+        $rangeA = [0, 5];
+        $this->assertTrue(Int::inRange($value, $rangeA[0], $rangeA[1]));
+        $this->assertTrue(Int::inRange($value, $rangeA[0], $rangeA[1], true));
+        $this->assertTrue(Int::inRange($value, $rangeA[0], $rangeA[1], false, true));
+        $this->assertTrue(Int::inRange($value, $rangeA[0], $rangeA[1], true, true));
+
+        $rangeB = [3, 9];
+        $this->assertTrue(Int::inRange($value, $rangeB[0], $rangeB[1]));
+        $this->assertFalse(Int::inRange($value, $rangeB[0], $rangeB[1], true));
+        $this->assertTrue(Int::inRange($value, $rangeB[0], $rangeB[1], false, true));
+        $this->assertFalse(Int::inRange($value, $rangeB[0], $rangeB[1], true, true));
+
+        $rangeC = [0, 3];
+        $this->assertTrue(Int::inRange($value, $rangeC[0], $rangeC[1]));
+        $this->assertTrue(Int::inRange($value, $rangeC[0], $rangeC[1], true));
+        $this->assertFalse(Int::inRange($value, $rangeC[0], $rangeC[1], false, true));
+        $this->assertFalse(Int::inRange($value, $rangeC[0], $rangeC[1], true, true));
+
+        $rangeD = [5, 9];
+        $this->assertFalse(Int::inRange($value, $rangeD[0], $rangeD[1]));
+        $this->assertFalse(Int::inRange($value, $rangeD[0], $rangeD[1], true));
+        $this->assertFalse(Int::inRange($value, $rangeD[0], $rangeD[1], false, true));
+        $this->assertFalse(Int::inRange($value, $rangeD[0], $rangeD[1], true, true));
+    }
+
+    /**
+     * Tests Int::inside()
+     * @depends test_inRange
+     */
+    public function test_inside()
+    {
+        $value = 3;
+        $rangeA = [0, 5];
+        $this->assertTrue(Int::inside($value, $rangeA[0], $rangeA[1]));
+
+        $rangeB = [3, 9];
+        $this->assertTrue(Int::inside($value, $rangeB[0], $rangeB[1]));
+
+        $rangeC = [0, 3];
+        $this->assertTrue(Int::inside($value, $rangeC[0], $rangeC[1]));
+
+        $rangeD = [5, 9];
+        $this->assertFalse(Int::inside($value, $rangeD[0], $rangeD[1]));
+    }
+
+    /**
+     * Tests Int::between()
+     * @depends test_inRange
+     */
+    public function test_between()
+    {
+        $value = 3;
+        $rangeA = [0, 5];
+        $this->assertTrue(Int::between($value, $rangeA[0], $rangeA[1]));
+
+        $rangeB = [3, 9];
+        $this->assertFalse(Int::between($value, $rangeB[0], $rangeB[1]));
+
+        $rangeC = [0, 3];
+        $this->assertFalse(Int::between($value, $rangeC[0], $rangeC[1]));
+
+        $rangeD = [5, 9];
+        $this->assertFalse(Int::between($value, $rangeD[0], $rangeD[1]));
+    }
+
+    /**
+     * Tests Int::inRange()
+     */
+    public function test_inRangeException01()
+    {
+        $value = 0.5;
+        $range = [0, 1];
+        $this->setExpectedException('\InvalidArgumentException');
+        Int::inRange($value, $range[0], $range[1]);
+    }
+
+    /**
+     * Tests Int::inRange()
+     */
+    public function test_inRangeException02()
+    {
+        $value = 1;
+        $range = [0.5, 1];
+        $this->setExpectedException('\InvalidArgumentException');
+        Int::inRange($value, $range[0], $range[1]);
+    }
+
+    /**
+     * Tests Int::inRange()
+     */
+    public function test_inRangeException03()
+    {
+        $value = 1;
+        $range = [0, 1.5];
+        $this->setExpectedException('\InvalidArgumentException');
+        Int::inRange($value, $range[0], $range[1]);
+    }
 }
--- src/axelitus/Base/Int.php
+++ src/axelitus/Base/Int.php
@@ -51,4 +51,74 @@ class Int extends PrimitiveInt
 
         return $rand;
     }
+
+    /**
+     * Tests if a value is in a range.
+     *
+     * Tests if a value is in a range. The function will correct the limits if inverted. The tested range
+     * can be set to have its lower and upper limits inclusive (bounds closed) or exclusive (bounds opened) using
+     * the $lowerExclusive and $upperExclusive parameters (all possible variations: ]a,b[ -or- ]a,b] -or- [a,b[
+     * -or- [a,b]).
+     *
+     * @param int  $value          The value to test in range.
+     * @param int  $lower          The range's lower limit.
+     * @param int  $upper          The range's upper limit.
+     * @param bool $lowerExclusive Whether the lower bound is exclusive.
+     * @param bool $upperExclusive Whether the upper bound is exclusive.
+     *
+     * @return bool Whether the value is in the given range given the bounds configurations.
+     * @throws \InvalidArgumentException
+     */
+    public static function inRange($value, $lower, $upper, $lowerExclusive = false, $upperExclusive = false)
+    {
+        if (!static::is($value) or !static::is($lower) or !static::is($upper)) {
+            throw new \InvalidArgumentException("The \$value, \$lower and \$upper parameters must be integers.");
+        }
+
+        $lowerLimit = min($lower, $upper);
+        if (!($lowerTest = ($lowerExclusive) ? $lowerLimit < $value : $lowerLimit <= $value)) {
+            return false;
+        }
+
+        $upperLimit = max($lower, $upper);
+        if (!($upperTest = ($upperExclusive) ? $upperLimit > $value : $upperLimit >= $value)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Tests if a value is inside a range.
+     *
+     * It's an alias for Int::inRange($value, $lower, $upper, false, false)
+     *
+     * @param int $value The value to test in range.
+     * @param int $lower The range's lower limit.
+     * @param int $upper The range's upper limit.
+     *
+     * @return bool Whether the value is in the given range given the bounds configurations.
+     * @see Int::inRange
+     */
+    public static function inside($value, $lower, $upper)
+    {
+        return static::inRange($value, $lower, $upper, false, false);
+    }
+
+    /**
+     * Tests if a value is between a range.
+     *
+     * It's an alias for Int::inRange($value, $lower, $upper, true, true)
+     *
+     * @param int $value The value to test in range.
+     * @param int $lower The range's lower limit.
+     * @param int $upper The range's upper limit.
+     *
+     * @return bool Whether the value is in the given range given the bounds configurations.
+     * @see Int::inRange
+     */
+    public static function between($value, $lower, $upper)
+    {
+        return static::inRange($value, $lower, $upper, true, true);
+    }
 }
