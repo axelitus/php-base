6aeebb113a39ced5fb587c18f793104687a2a881
Axel Pardemann <axelitusdev@gmail.com>
2013-11-21 22:36:49 -0600

    Added some boolean operations and it's tests.

--- res/tests/axelitus/Base/TestsBoolean.php
+++ res/tests/axelitus/Base/TestsBoolean.php
@@ -35,4 +35,66 @@ class TestsBoolean extends TestCase
         $this->assertFalse(Boolean::is("1"), "The value \"1\" is incorrectly recognized as a boolean.");
         $this->assertFalse(Boolean::is("0"), "The value \"0\" is incorrectly recognized as a boolean.");
     }
+
+    /**
+     * Tests Boolean::doNot()
+     */
+    public function test_doNot()
+    {
+        $this->assertTrue(Boolean::doNot(false));
+        $this->assertFalse(Boolean::doNot(true));
+    }
+
+    /**
+     * Tests Boolean::doAnd()
+     */
+    public function test_doAnd()
+    {
+        $this->assertTrue(Boolean::doAnd(true, true, true, true));
+        $this->assertFalse(Boolean::doAnd(true, false, true, true));
+        $this->assertTrue(Boolean::doAnd([true, true, true, true], true));
+        $this->assertFalse(Boolean::doAnd([true, true, true, true], false));
+        $this->assertFalse(Boolean::doAnd([true, true, false, true], true));
+    }
+
+    /**
+     * Tests Boolean::doOr()
+     */
+    public function test_doOr()
+    {
+        $this->assertTrue(Boolean::doOr(true, true, true, true));
+        $this->assertTrue(Boolean::doOr(false, false, true, false));
+        $this->assertFalse(Boolean::doOr(false, false, false, false));
+        $this->assertTrue(Boolean::doOr([false, false, false, false], true));
+        $this->assertFalse(Boolean::doOr([false, false, false, false], false));
+        $this->assertTrue(Boolean::doOr([false, false, true, false], false));
+    }
+
+    /**
+     * Tests Boolean::doEq()
+     */
+    public function test_doEq()
+    {
+        $this->assertFalse(Boolean::doEq(true, false));
+        $this->assertFalse(Boolean::doEq(false, true));
+        $this->assertTrue(Boolean::doEq(true, true));
+        $this->assertTrue(Boolean::doEq(false, false));
+
+        $this->assertEquals([true, false, true], Boolean::doEq([true, true, false], [true, false, false]));
+    }
+
+    /**
+     * Tests Boolean::doXor()
+     *
+     * @depends test_doEq
+     */
+    public function test_doXor()
+    {
+        $this->assertTrue(Boolean::doXor(true, false));
+        $this->assertTrue(Boolean::doXor(false, true));
+        $this->assertFalse(Boolean::doXor(true, true));
+        $this->assertFalse(Boolean::doXor(false, false));
+
+        $this->assertEquals([false, true, false], Boolean::doXor([true, true, false], [true, false, false]));
+    }
 }
--- src/axelitus/Base/Boolean.php
+++ src/axelitus/Base/Boolean.php
@@ -23,4 +23,173 @@ use axelitus\Base\Primitives\Boolean\PrimitiveBoolean;
  */
 class Boolean extends PrimitiveBoolean
 {
+    /**
+     * Applies the boolean NOT operation to the given input value.
+     *
+     * @param bool|PrimitiveBoolean $input The value to apply the NOT operation to.
+     *
+     * @return bool Returns true if $input was false, and false if $input was true.
+     * @throws \InvalidArgumentException
+     */
+    public static function doNot($input)
+    {
+        try {
+            $input = static::native($input);
+        } catch (\InvalidArgumentException $ex) {
+            throw new \InvalidArgumentException("The \$input parameter must be a boolean or instance derived from PrimitiveBoolean.");
+        }
+
+        return !$input;
+    }
+
+    /**
+     * Applies the boolean AND operation to the given input values.
+     *
+     * @param bool|PrimitiveBoolean|array $input,... The input booleans or array of booleans to apply the AND operation to.
+     *
+     * @return bool Returns true if all values are true, false otherwise.
+     * @throws \InvalidArgumentException
+     */
+    public static function doAnd($input)
+    {
+        $chain = true;
+        $args = func_get_args();
+
+        foreach ($args as $arg) {
+            try {
+                if (is_array($arg)) {
+                    foreach ($arg as $b) {
+                        $chain &= static::native($b);
+                    }
+                } else {
+                    $chain &= $arg;
+                }
+
+                if (!$chain) {
+                    return false;
+                }
+            } catch (\InvalidArgumentException $ex) {
+                throw new \InvalidArgumentException("The parameters must be booleans or instances derived from PrimitiveBoolean.");
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Applies the boolean OR operation to the given input values.
+     *
+     * @param bool|PrimitiveBoolean|array $input,... The input booleans or array of booleans to apply the OR operation to.
+     *
+     * @return bool Returns true if at least one of the values is true, false otherwise.
+     * @throws \InvalidArgumentException
+     */
+    public static function doOr($input)
+    {
+        $chain = false;
+        $args = func_get_args();
+
+        foreach ($args as $arg) {
+            try {
+                if (is_array($arg)) {
+                    foreach ($arg as $b) {
+                        $chain |= static::native($b);
+                    }
+                } else {
+                    $chain |= $arg;
+                }
+
+                if ($chain) {
+                    return true;
+                }
+            } catch (\InvalidArgumentException $ex) {
+                throw new \InvalidArgumentException("The parameters must be booleans or instances derived from PrimitiveBoolean.");
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Applies the boolean EQ (equality) operation to the given $a and $b values or array of values.
+     *
+     * @param bool|PrimitiveBoolean|array $a The first EQ operand or array of operands.
+     * @param bool|PrimitiveBoolean|array $b The second EQ operand or array of operands.
+     *
+     * @return bool|array Returns true if $a and $b are equal, false otherwise (or an array of results).
+     * @throws \InvalidArgumentException
+     */
+    public static function doEq($a, $b)
+    {
+        $ret = [];
+
+        try {
+            $source = static::native($a);
+        } catch (\InvalidArgumentException $ex) {
+            if (!is_array($a)) {
+                throw new \InvalidArgumentException("The \$a parameter must be a boolean or an instance derived from PrimitiveBoolean or an array of booleans or instances derived from PrimitiveBoolean.");
+            }
+
+            $source = $a;
+        }
+
+        try {
+            $bind = static::native($b);
+        } catch (\InvalidArgumentException $ex) {
+            if (!is_array($b)) {
+                throw new \InvalidArgumentException("The \$b parameter must be a boolean or an instance derived from PrimitiveBoolean or an array of booleans or instances derived from PrimitiveBoolean.");
+            }
+
+            $bind = $b;
+        }
+
+        if (is_array($source)) {
+            if (is_array($bind)) {
+                if (($count = count($source)) !== count($bind)) {
+                    throw new \InvalidArgumentException("The \$a and \$b arrays must have the same amount of elements.");
+                }
+
+                for ($i = 0; $i < $count; $i++) {
+                    $ret[] = ($bind[$i] == $source[$i]);
+                }
+            } else {
+                foreach ($source as $s) {
+                    $ret[] = ($bind == $s);
+                }
+            }
+        } else {
+            if (is_array($bind)) {
+                foreach ($bind as $b) {
+                    $ret[] = ($b == $source);
+                }
+            } else {
+                $ret = ($bind == $source);
+            }
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Applies the boolean XOR operation to the given $a and $b values or array of values.
+     *
+     * @param bool|PrimitiveBoolean|array $a The first XOR operand or array of operands.
+     * @param bool|PrimitiveBoolean|array $b The second XOR operand or array of operands.
+     *
+     * @return bool Returns true if $a and $b are different, false otherwise (or an array of results).
+     * @throws \InvalidArgumentException
+     */
+    public static function doXor($a, $b)
+    {
+        $ret = static::doEq($a, $b);
+        if (is_array($ret)) {
+            foreach ($ret as &$r) {
+                $r = static::doNot($r);
+            }
+        } else {
+            $ret = static::doNot($ret);
+        }
+
+        return $ret;
+    }
 }
