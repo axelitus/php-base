3aaa5ae6253a1a7898597176dd806f0f8de6d675
Axel Pardemann <axelitusdev@gmail.com>
2013-11-21 18:06:11 -0600

    Bumped version to 0.3

--- res/tests/axelitus/Base/TestCase.php
+++ res/tests/axelitus/Base/TestCase.php
@@ -3,7 +3,7 @@
  * Part of composer package: axelitus/base
  *
  * @package     axelitus\Base
- * @version     0.2
+ * @version     0.3
  * @author      Axel Pardemann (axelitusdev@gmail.com)
  * @license     MIT License
  * @copyright   2013 - Axel Pardemann
