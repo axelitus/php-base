ba29ea7f5d670ccc8a12f09663438d23e89a893b
Axel Pardemann <axelitusdev@gmail.com>
2013-11-28 21:49:42 -0600

    Added docblocks.

--- res/tests/axelitus/Base/TestCase.php
+++ res/tests/axelitus/Base/TestCase.php
@@ -19,13 +19,25 @@ namespace axelitus\Base\Tests;
  */
 abstract class TestCase extends \PHPUnit_Framework_TestCase
 {
+    /** @var string $class */
     protected $class;
 
+    /**
+     * Setup function to configure the tests.
+     */
     public function setUp()
     {
         $this->class = str_replace('Tests\Tests', '', get_called_class());
     }
 
+    /**
+     * Gets a non-public method from a class and sets it as accessible to run tests.
+     *
+     * @param string $class      The class that contains the method to get.
+     * @param string $methodName The method to get.
+     *
+     * @return \ReflectionMethod The method.
+     */
     protected static function getNonPublicMethod($class, $methodName)
     {
         $ref = new \ReflectionClass($class);
@@ -34,6 +46,15 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
         return $method;
     }
 
+    /**
+     * Executes a non-public method from a class.
+     *
+     * @param string $class      The class that contains the method to execute.
+     * @param string $methodName The method to execute.
+     * @param array  $args       The arguments to pass to the method execution.
+     *
+     * @return mixed The result of the execution.
+     */
     protected static function execNonPublicMethod($class, $methodName, array $args = [])
     {
         $method = static::getNonPublicMethod($class, $methodName);
