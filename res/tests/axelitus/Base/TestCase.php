09230d4d588c9bf84f17316f0277e47094d38070
Axel Pardemann <axelitusdev@gmail.com>
2013-12-06 12:55:36 -0600

    Missing commit for the modified TestCase base class to allow for testing non-public properties.

--- res/tests/axelitus/Base/TestCase.php
+++ res/tests/axelitus/Base/TestCase.php
@@ -31,33 +31,49 @@ abstract class TestCase extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * Gets a non-public method from a class and sets it as accessible to run tests.
+     * Gets a non-public property from a class or instance and sets it as accessible to run tests.
      *
-     * @param string $class      The class that contains the method to get.
-     * @param string $methodName The method to get.
+     * @param string $classOrInstance The class or instance that contains the property to get.
+     * @param string $propertyName    The property to get.
+     *
+     * @return \ReflectionProperty The property.
+     */
+    protected static function getNonPublicProperty($classOrInstance, $propertyName)
+    {
+        $ref = new \ReflectionClass($classOrInstance);
+        $property = $ref->getProperty($propertyName);
+        $property->setAccessible(true);
+        return $property;
+    }
+
+    /**
+     * Gets a non-public method from a class or instance and sets it as accessible to run tests.
+     *
+     * @param string $classOrInstance The class or instance that contains the method to get.
+     * @param string $methodName      The method to get.
      *
      * @return \ReflectionMethod The method.
      */
-    protected static function getNonPublicMethod($class, $methodName)
+    protected static function getNonPublicMethod($classOrInstance, $methodName)
     {
-        $ref = new \ReflectionClass($class);
+        $ref = new \ReflectionClass($classOrInstance);
         $method = $ref->getMethod($methodName);
         $method->setAccessible(true);
         return $method;
     }
 
     /**
-     * Executes a non-public method from a class.
+     * Executes a non-public method from a class or instance.
      *
-     * @param string $class      The class that contains the method to execute.
-     * @param string $methodName The method to execute.
-     * @param array  $args       The arguments to pass to the method execution.
+     * @param string $classOrInstance The class or instance that contains the method to execute.
+     * @param string $methodName      The method to execute.
+     * @param array  $args            The arguments to pass to the method execution.
      *
      * @return mixed The result of the execution.
      */
-    protected static function execNonPublicMethod($class, $methodName, array $args = [])
+    protected static function execNonPublicMethod($classOrInstance, $methodName, array $args = [])
     {
-        $method = static::getNonPublicMethod($class, $methodName);
+        $method = static::getNonPublicMethod($classOrInstance, $methodName);
         return $method->invokeArgs(null, $args);
     }
 }
