8b01f67f259a532270cc2ff008026fa4236677c1
Axel Pardemann <axelitusdev@gmail.com>
2013-12-06 13:55:03 -0600

    Added the Arr class to instantiate dot-notated array objects.

---
+++ res/tests/axelitus/Base/TestsArr.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * PHP Package: axelitus/base - Primitive operations and helpers.
+ *
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/axelitus/base
+ * @license     MIT License (@see LICENSE.md)
+ * @package     axelitus\Base
+ * @version     0.5
+ */
+
+namespace axelitus\Base\Tests;
+
+use axelitus\Base\Arr;
+
+/**
+ * Class TestsArr
+ *
+ * @package axelitus\Base
+ */
+class TestsArr extends TestCase
+{
+    //region Properties
+
+    /** @var \axelitus\Base\Arr */
+    private $arr;
+
+    //endregion
+
+    //region SetUp
+
+    public function setUp()
+    {
+        $this->arr = new Arr();
+    }
+
+    //endregion
+
+    //region Constructors
+
+    public function testNew()
+    {
+        $data = static::getNonPublicProperty($this->arr, 'data')->getValue($this->arr);
+        $this->assertInternalType('array', $data);
+        $this->assertEquals([], $data);
+
+        $a = ['first' => 'value', 'second' => ['lvl' => 'lvl value']];
+        $this->arr = new Arr($a);
+        $data = static::getNonPublicProperty($this->arr, 'data')->getValue($this->arr);
+        $this->assertInternalType('array', $data);
+        $this->assertEquals($a, $data);
+    }
+
+    //endregion
+
+    //region Get & Set
+
+    public function testGet()
+    {
+        static::getNonPublicProperty($this->arr, 'data')->setValue($this->arr, ['first' => 'value', 'second' => ['lvl' => ['opt' => 'other value']]]);
+
+        $this->assertEquals('value', $this->arr->get('first'));
+        $this->assertEquals('other value', $this->arr->get('second.lvl.opt'));
+        $this->assertEquals(null, $this->arr->get('third'));
+    }
+
+    public function testSet()
+    {
+        $this->arr->set('first', 'value');
+        $this->arr->set('second.lvl.opt', 'other value');
+
+        $data = static::getNonPublicProperty($this->arr, 'data')->getValue($this->arr);
+        $this->assertArrayHasKey('first', $data);
+        $this->assertEquals('value', $data['first']);
+        $this->assertArrayHasKey('second', $data);
+        $this->assertArrayHasKey('lvl', $data['second']);
+        $this->assertArrayHasKey('opt', $data['second']['lvl']);
+        $this->assertEquals('other value', $data['second']['lvl']['opt']);
+    }
+
+    public function testDelete()
+    {
+        static::getNonPublicProperty($this->arr, 'data')->setValue($this->arr, ['first' => 'value', 'second' => ['lvl' => ['opt' => 'other value']], 'third' => ['sub' => 'sub value']]);
+
+        $this->assertTrue($this->arr->delete('first'));
+        $data = static::getNonPublicProperty($this->arr, 'data')->getValue($this->arr);
+        $this->assertArrayNotHasKey('first', $data);
+
+        $this->assertFalse($this->arr->delete('fourth'));
+
+        $this->assertEquals(['second' => true, 'third' => true, 'fifth' => false], $this->arr->delete(['second', 'third', 'fifth']));
+        $data = static::getNonPublicProperty($this->arr, 'data')->getValue($this->arr);
+        $this->assertArrayNotHasKey('second', $data);
+        $this->assertArrayNotHasKey('third', $data);
+        $this->assertEquals([], $data);
+    }
+
+    //endregion
+
+    //region Matches
+
+    public function testHas()
+    {
+        static::getNonPublicProperty($this->arr, 'data')->setValue($this->arr, ['first' => 'value', 'second' => ['lvl' => ['opt' => 'other value']], 'third' => ['sub' => 'sub value']]);
+
+        $this->assertTrue($this->arr->has('first'));
+        $this->assertTrue($this->arr->has('second.lvl'));
+        $this->assertFalse($this->arr->has('fourth'));
+        $this->assertEquals(['first' => true, 'third.sub' => true, 'fifth' => false], $this->arr->has(['first', 'third.sub', 'fifth']));
+    }
+
+    //endregion
+
+    //region Implements \ArrayAccess
+
+    /**
+     * @depends testGet
+     * @depends testSet
+     * @depends testDelete
+     * @depends testHas
+     */
+    public function testArrayAccess()
+    {
+        // test offsetGet and offsetSet
+        $this->assertEquals(null, $this->arr['first.sub.val']);
+        $this->arr['first.sub.val'] = 'value';
+        $this->assertEquals('value', $this->arr['first.sub.val']);
+
+        // test offsetExists and offsetUnset
+        $this->assertTrue(isset($this->arr['first.sub.val']));
+        unset($this->arr['first.sub.val']);
+        $this->assertFalse(isset($this->arr['first.sub.val']));
+    }
+
+    //endregion
+
+    //region Implements \Countable
+
+    public function testCountable()
+    {
+        $this->assertEquals(0, count($this->arr));
+        $this->assertEquals(false, $this->arr->count('first'));
+
+        static::getNonPublicProperty($this->arr, 'data')->setValue($this->arr, ['first' => 'value', 'second' => ['lvl' => ['opt' => ['value 1', 'value 2', 'value 3', 'value 4', 'value 5']]], 'third' => ['sub' => 'sub value']]);
+        $this->assertEquals(1, $this->arr->count('first'));
+        $this->assertEquals(1, $this->arr->count('second.lvl'));
+        $this->assertEquals(5, $this->arr->count('second.lvl.opt'));
+
+        $this->assertEquals(['first' => 1, 'second.lvl.opt' => 5, 'third' => 1, 'fourth' => false], $this->arr->count(['first', 'second.lvl.opt', 'third', 'fourth']));
+    }
+
+    public function testCountableEx01()
+    {
+        $this->setExpectedException('\InvalidArgumentException', "The \$key parameter must be int or string (or array of them).");
+        $this->arr->count(false);
+    }
+
+    //endregion
+}
---
+++ src/axelitus/Base/Arr.php
@@ -0,0 +1,212 @@
+<?php
+/**
+ * PHP Package: axelitus/base - Primitive operations and helpers.
+ *
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/axelitus/base
+ * @license     MIT License (@see LICENSE.md)
+ * @package     axelitus\Base
+ * @version     0.5
+ */
+
+namespace axelitus\Base;
+
+/**
+ * Class Arr
+ *
+ * Dot-Notated Array object.
+ *
+ * @package axelitus\Base
+ */
+class Arr implements \ArrayAccess, \Countable
+{
+    //region Properties
+
+    /**
+     * @var array The internal dot-notated array.
+     */
+    private $data = [];
+
+    //endregion
+
+    //region Constructors
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+    }
+
+    //endregion
+
+    //region Get & Set
+
+    /**
+     * Gets a value from the dot-notated internal array.
+     *
+     * @param int|string|array $key     The key of the item's value to get or an array of keys.
+     * @param mixed            $default A default value to return if the item is not found.
+     *
+     * @return mixed The value of the item if found, the default value otherwise.
+     * @see DotArr::get
+     */
+    public function get($key, $default = null)
+    {
+        return DotArr::get($this->data, $key, $default);
+    }
+
+    /**
+     * Sets a value to the dot-notated internal array.
+     *
+     * @param int|string|array $key   The key of the item to be set or an array of key=>value pairs.
+     * @param mixed            $value The value to be set to the item.
+     *
+     * @see DotArr::set
+     */
+    public function set($key, $value = null)
+    {
+        DotArr::set($this->data, $key, $value);
+    }
+
+    /**
+     * @param $key
+     *
+     * @return array|bool
+     */
+    public function delete($key)
+    {
+        return DotArr::delete($this->data, $key);
+    }
+
+    //endregion
+
+    //region Matches
+
+    /**
+     * @param $key
+     *
+     * @return array|bool
+     */
+    public function has($key)
+    {
+        return DotArr::keyExists($this->data, $key);
+    }
+
+    //endregion
+
+    //region Implements \ArrayAccess
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Whether a offset exists
+     * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+     *
+     * @param mixed $offset <p>
+     *                      An offset to check for.
+     *                      </p>
+     *
+     * @return boolean true on success or false on failure.
+     * </p>
+     * <p>
+     * The return value will be casted to boolean if non-boolean was returned.
+     */
+    public function offsetExists($offset)
+    {
+        return $this->has($offset);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to retrieve
+     * @link http://php.net/manual/en/arrayaccess.offsetget.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to retrieve.
+     *                      </p>
+     *
+     * @return mixed Can return all value types.
+     */
+    public function offsetGet($offset)
+    {
+        return $this->get($offset);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to set
+     * @link http://php.net/manual/en/arrayaccess.offsetset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to assign the value to.
+     *                      </p>
+     * @param mixed $value  <p>
+     *                      The value to set.
+     *                      </p>
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->set($offset, $value);
+    }
+
+    /**
+     * (PHP 5 &gt;= 5.0.0)<br/>
+     * Offset to unset
+     * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+     *
+     * @param mixed $offset <p>
+     *                      The offset to unset.
+     *                      </p>
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        $this->delete($offset);
+    }
+
+    //endregion
+
+    //region Implements \Countable
+
+    /**
+     * (PHP 5 &gt;= 5.1.0)<br/>
+     * Count elements of an object
+     * @link http://php.net/manual/en/countable.count.php
+     *
+     * @param null|int|string|array $key The key to get the count from.
+     *
+     * @throws \InvalidArgumentException
+     * @return int|array The custom count as an integer.
+     * </p>
+     * <p>
+     * The return value is cast to an integer.
+     */
+    public function count($key = null)
+    {
+        if (is_null($key)) {
+            return count($this->data);
+        }
+
+        if (is_array($key)) {
+            $return = [];
+            foreach ($key as $k) {
+                $return[$k] = $this->count($k);
+            }
+            return $return;
+        }
+
+        if (!Int::is($key) && !Str::is($key)) {
+            throw new \InvalidArgumentException("The \$key parameter must be int or string (or array of them).");
+        }
+
+        if (!$this->has($key)) {
+            return false;
+        }
+
+        return count($this->get($key));
+    }
+
+    //endregion
+}
