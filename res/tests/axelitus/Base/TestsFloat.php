ffe853ba51a0fde99a020c4ef46c9794ea526060
Axel Pardemann <axelitusdev@gmail.com>
2013-12-09 12:17:05 -0600

    Fixed license inline docblock tag.

--- res/tests/axelitus/Base/TestsFloat.php
+++ res/tests/axelitus/Base/TestsFloat.php
@@ -5,7 +5,7 @@
  * @author      Axel Pardemann (axelitusdev@gmail.com)
  * @copyright   2013 - Axel Pardemann
  * @link        http://axelitus.mx/projects/axelitus/base
- * @license     MIT License (@see LICENSE.md)
+ * @license     MIT License ({@link LICENSE.md})
  * @package     axelitus\Base
  * @version     0.5
  */
