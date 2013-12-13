5816e2bb05ff25f436dce3152f495facb6896e96
Axel Pardemann <axelitusdev@gmail.com>
2013-12-13 16:41:21 -0600

    Added the Traversor class to handle array loop manipulation.

---
+++ res/tests/axelitus/Base/TestsTraversor.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * PHP Package: axelitus/base - Primitive operations and helpers.
+ *
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/axelitus/base
+ * @license     MIT License ({@link LICENSE.md})
+ * @package     axelitus\Base
+ * @version     0.6.0
+ */
+
+namespace axelitus\Base\Tests;
+
+use axelitus\Base\Traversor;
+
+/**
+ * Class TestsTraversor
+ *
+ * @package axelitus\Base
+ */
+class TestsTraversor extends TestCase
+{
+    public function testRun()
+    {
+        $arr = [2, 3, 4, 5];
+        $this->assertEquals($arr, Traversor::run($arr));
+
+        $arr = [2, 3, 4, 5];
+        $this->assertEquals(
+            [4, 9, 16, 25],
+            Traversor::run(
+                $arr,
+                function ($value) {
+                    return pow($value, 2);
+                }
+            )
+        );
+
+        $arr = [2, 3, 4, 5];
+        $this->assertEquals(
+            [0, 3, 8, 15],
+            Traversor::run(
+                $arr,
+                function ($value, $key) {
+                    return $key * $value;
+                }
+            )
+        );
+
+        $arr = [2, 3, 4, 5];
+        $this->assertEquals(
+            [3 => 10, 4 => 15, 5 => 20, 6 => 25],
+            Traversor::run(
+                $arr,
+                function ($value, &$key) {
+                    $key += 3;
+                    return $value * 5;
+                }
+            )
+        );
+
+        // Test by reference array item callback manipulation
+        $arr = [2, 3, 4, 5];
+        $this->assertEquals(
+            [0 => 'deleted', 1 => 'deleted', 2 => 'deleted', 3 => 'deleted'],
+            Traversor::run(
+                $arr,
+                function ($value, $key, &$arr) {
+                    unset($arr[$key]);
+                    return 'deleted';
+                }
+            )
+        );
+        $this->assertEquals([], $arr);
+
+        // Test by reference array post item callback manipulation
+        $arr = [2, 3, 4, 5];
+        $this->assertEquals(
+            $arr,
+            Traversor::run(
+                $arr,
+                function ($value) {
+                    return $value;
+                },
+                function (&$arr, $result) {
+                    for ($i = 0; $i < count($arr); $i++) {
+                        $arr[$i] = $result;
+                    }
+
+                    return $result;
+                }
+            )
+        );
+    }
+}
---
+++ src/axelitus/Base/Traversor.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * PHP Package: axelitus/base - Primitive operations and helpers.
+ *
+ * @author      Axel Pardemann (axelitusdev@gmail.com)
+ * @copyright   2013 - Axel Pardemann
+ * @link        http://axelitus.mx/projects/axelitus/base
+ * @license     MIT License ({@link LICENSE.md})
+ * @package     axelitus\Base
+ * @version     0.6.0
+ */
+
+namespace axelitus\Base;
+
+/**
+ * Class Traversor
+ *
+ *
+ *
+ * @package axelitus\Base
+ */
+class Traversor
+{
+    public static function run(array &$arr, callable $itemCallback = null, callable $resultCallback = null)
+    {
+        if (is_null($itemCallback)) {
+            return $arr;
+        } else {
+            $result = [];
+            foreach ($arr as $key => $value) {
+                // Run the callback and store the result in a variable so that we allow the key to be modified by the callback.
+                // also pass the original array if one needs to unset variables from the original array.
+                $r = $itemCallback($value, $key, $arr);
+
+                // Store the item callback's result with the (new) key.
+                $result[$key] = $r;
+            }
+
+            if (!is_null($resultCallback)) {
+                // Allow for the original array to be modified with the result array.
+                return $resultCallback($arr, $result);
+            }
+
+            return $result;
+        }
+    }
+}
