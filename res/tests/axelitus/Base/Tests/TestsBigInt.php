f8a91a08b6ea94a2508c2dc04b7dc7252f09c7d2
Axel Pardemann <axelitusdev@gmail.com>
2015-02-20 16:37:37 -0600

    Bumped version to 0.8.1 and updated copyright year.

--- res/tests/axelitus/Base/Tests/TestsBigInt.php
+++ res/tests/axelitus/Base/Tests/TestsBigInt.php
@@ -3,11 +3,11 @@
  * PHP Package: axelitus/base - Primitive operations and helpers.
  *
  * @author      Axel Pardemann (axelitusdev@gmail.com)
- * @copyright   2013 - Axel Pardemann
+ * @copyright   2015 - Axel Pardemann
  * @link        http://axelitus.mx/projects/axelitus/base
  * @license     MIT License ({@link LICENSE.md})
  * @package     axelitus\Base
- * @version     0.8.0
+ * @version     0.8.1
  */
 
 namespace axelitus\Base\Tests;
