5500f109b1abd7bed57e29988437d1de10824ce9
Axel Pardemann <axelitusdev@gmail.com>
2016-02-05 16:22:20 -0600

    Updated the copyright year.

--- src/Float.php
+++ src/Float.php
@@ -3,7 +3,7 @@
  * PHP Package: axelitus/base - Primitive operations and helpers.
  *
  * @author      Axel Pardemann (axelitusdev@gmail.com)
- * @copyright   2015 - Axel Pardemann
+ * @copyright   2016 - Axel Pardemann
  * @link        http://axelitus.mx/projects/axelitus/base
  * @license     MIT License ({@link LICENSE.md})
  * @package     axelitus\Base
